259711ab169f8babcde3cf1d07103790ed37284e
panaaj <38519157+panaaj@users.noreply.github.com>
2026-01-31 17:33:01 +1030

    added delta processing to set sensors.ais.status

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,44 @@ import {
   Path,
   PathValue,
   Plugin,
+  SKVersion,
   ServerAPI,
   SubscribeMessage,
   Update,
   hasValues
 } from '@signalk/server-api'
 
+interface SKAisApp extends ServerAPI {}
 
-export interface SKAisApp extends ServerAPI {}
+interface TargetDef {
+  lastPosition: number
+  msgCount: number
+}
+
+interface ClassDefault {
+  confirmAfterMsgs: number
+  confirmMaxAge: number // msecs
+  lostAfter: number // msecs
+  removeAfter: number // msecs
+  interpHz: number // msecs
+}
+
+const AIS_CLASS_DEFAULTS: Record<string, ClassDefault> = {
+  A: {
+    confirmAfterMsgs: 2,
+    confirmMaxAge: 30000,
+    lostAfter: 60000,
+    removeAfter: 180000,
+    interpHz: 1000
+  },
+  B: {
+    confirmAfterMsgs: 3,
+    confirmMaxAge: 90000,
+    lostAfter: 180000,
+    removeAfter: 600000,
+    interpHz: 500
+  }
+}
 
 const CONFIG_SCHEMA = {
   properties: {}
@@ -37,6 +67,8 @@ module.exports = (server: SKAisApp): Plugin => {
   }
 
   let settings: any = {}
+  let self: string = ''
+  let targets: Map<Context, TargetDef> = new Map()
 
   const doStartup = (options: any) => {
     try {
@@ -77,62 +109,123 @@ module.exports = (server: SKAisApp): Plugin => {
     server.debug('Initialising ....')
     // setup subscriptions
     initSubscriptions()
+    self = server.getPath('self')
   }
 
   // register DELTA stream message handler
   const initSubscriptions = () => {
-    server.debug('Initialising Stream Subscription....')
+    server.debug('Initialising Stream Subscriptions....')
+    const subDef = [
+      {
+        path: 'navigation.position' as Path,
+        period: 500
+      }
+    ]
+    const subs: SubscribeMessage[] = [
+      {
+        context: 'vessels.*' as Context,
+        subscribe: subDef
+      },
+      {
+        context: 'atons.*' as Context,
+        subscribe: subDef
+      },
+      {
+        context: 'shore.basestations.*' as Context,
+        subscribe: subDef
+      },
+      {
+        context: 'sar.*' as Context,
+        subscribe: subDef
+      },
+      {
+        context: 'aircraft.*' as Context,
+        subscribe: subDef
+      }
+    ]
+    subs.forEach((s) => {
+      server.subscriptionmanager.subscribe(s, subscriptions, onError, onMessage)
+    })
+  }
 
-    const subscription: SubscribeMessage = {
-      context: 'vessels.*' as Context,
-      subscribe: [
-        {
-          path: '' as Path,
-          period: 500
-        }
-      ]
+  /**
+   * Delta message handler
+   * @param delta Delta message
+   */
+  const onMessage = (delta: Delta) => {
+    if (!delta.updates) {
+      return
     }
 
-    server.subscriptionmanager.subscribe(
-      subscription,
-      subscriptions,
-      (error) => {
-        server.error(`${plugin.id} Error: ${error}`)
-      },
-      (delta: Delta) => {
-        if (!delta.updates) {
-          return
-        }
-        const context = delta.context
-        delta.updates.forEach((u: Update) => {
-          if (!hasValues(u)) {
-            return
-          }
-          u.values.forEach((v: PathValue) => {
-            if (v.path === '' && v.value && 'mmsi' in (v.value as object)) {
-              // if doesn't already have xxx property
-              const vf = server.getPath(`${context}.xxx`)
-              if (!vf) {
-                server.handleMessage(plugin.id, {
-                  context: context,
-                  updates: [
-                    {
-                      values: [
-                        {
-                          path: 'xxx' as Path,
-                          value: 'new value'
-                        }
-                      ]
-                    }
-                  ]
-                })
-                
-              }
-            }
-          })
-        })
+    if (delta.context === self) {
+      console.log('self:', self)
+      return
+    }
+    delta.updates.forEach((u: Update) => {
+      if (!hasValues(u)) {
+        return
       }
-    )
+      u.values.forEach((v: PathValue) => {
+        if (v.path === 'navigation.position') {
+          if (!targets.has(delta.context as Context)) {
+            targets.set(delta.context as Context, {
+              lastPosition: 0,
+              msgCount: 0
+            })
+          }
+          processTarget(delta.context as Context)
+        }
+      })
+    })
+  }
+
+  /** Handle subscription error */
+  const onError = (error: unknown) => {
+    server.error(`${plugin.id} Error: ${error}`)
+  }
+
+  /** Process target after position message */
+  const processTarget = (context: Context) => {
+    const target: TargetDef = targets.get(context) as TargetDef
+    if (!target) return
+
+    let p = server.getPath(`${context}.sensors.ais.class`)
+    // process class values and default to A if missing or invalid
+    let aisClass = !p?.value ? 'A' : p.value.length > 1 ? p.value[0] : p.value
+    aisClass = Object.keys(AIS_CLASS_DEFAULTS).includes(aisClass)
+      ? aisClass
+      : 'A'
+
+    const tDiff = Date.now() - target?.lastPosition
+    // interpHz threshold met?
+    if (tDiff < AIS_CLASS_DEFAULTS[aisClass].interpHz) return
+
+    target.lastPosition = Date.now()
+    target.msgCount++
+
+    // confirmMsg threshold met?
+    if (target.msgCount >= AIS_CLASS_DEFAULTS[aisClass].confirmAfterMsgs) {
+      //server.debug('*** confirmed', target.msgCount, context, aisClass)
+      target.msgCount = 0
+      server.handleMessage(
+        plugin.id,
+        {
+          context: context,
+          updates: [
+            {
+              values: [
+                {
+                  path: 'sensors.ais.status' as Path,
+                  value: 'confirmed'
+                }
+              ]
+            }
+          ]
+        },
+        SKVersion.v1
+      )
+    }
+    targets.set(context, target)
   }
 
   return plugin
